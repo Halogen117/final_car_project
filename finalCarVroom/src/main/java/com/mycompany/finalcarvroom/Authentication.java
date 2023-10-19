32b42192bc7b0c92e9f378d1aece6d11c7f534cc
Russel Tan Jun Hong <masterchiefplaysminecraft117@gmail.com>
2023-10-19 17:10:28 +0800

    UPLOAD CHANGES

--- finalCarVroom/src/main/java/com/mycompany/finalcarvroom/Authentication.java
+++ finalCarVroom/src/main/java/com/mycompany/finalcarvroom/Authentication.java
@@ -4,7 +4,6 @@
  */
 package com.mycompany.finalcarvroom;
 
-import java.util.Base64;
 import java.sql.*;
 import java.util.Base64;
 /**
@@ -13,9 +12,7 @@ import java.util.Base64;
  */
 public class Authentication {
     ConnectDB create = new ConnectDB();
-    ////////////////////// LOGIN ////////////////////////
-    public String login(String email, String password) {
-                String userName = "";
+    public void login(String email, String password) {
 		try {
                     Connection connectDB = create.getConnection();
                     Statement statement = connectDB.createStatement();
@@ -23,28 +20,22 @@ public class Authentication {
                     String sql = "SELECT * FROM user_DB WHERE email = '" + email +"' AND password = '" + password +"'";
 
                     ResultSet resultSet = statement.executeQuery(sql);
-                    
+
                     if(resultSet.next()) {
                             System.out.println("Login Successful");
-                            userName = resultSet.getString("user_ID");
-                            connectDB.close();
-                            return userName;
                             //activate "Login Successful" display message code here
                             //Close Guest Page and
                             //Display Member Page
                     }
                     else {
                             System.out.println("Authentication Failed");
-                            connectDB.close();
-                            return userName;
                             //activate "Authentication Failed" display message code here
                             //Reset password textbox
                     }
-                    
+                    connectDB.close();
 		}catch(SQLException e) {
 			e.printStackTrace();
 		}
-                return userName;
 	}
 	
 	
@@ -132,6 +123,7 @@ public class Authentication {
         }
         return userDetails[0];
         }
+
         public String encoder_link(String email){
             String [] userDetails = new String[2];
             String baser = "";
